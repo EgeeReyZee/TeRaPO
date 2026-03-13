3aa0d235a8770612b4e35c7643835a233b612f7a
EgeeReyZee <karshiboev05@bk.ru>
2026-03-13 15:26:24 +0700

    Gemm 1 Thread

--- gemm/main.c
+++ gemm/main.c
@@ -44,10 +44,23 @@ void print_matrix(Matrix m) {
     }
 }
 
+void gemm_single_threaded(Matrix A, Matrix B, Matrix C) {
+    for (int i = 0; i < A.rows; i++) {
+        for (int j = 0; j < B.cols; j++) {
+            double sum = 0.0;
+            for (int k = 0; k < A.cols; k++) {
+                sum += A.data[i * A.cols + k] * B.data[k * B.cols + j];
+            }
+            C.data[i * C.cols + j] = sum;
+        }
+    }
+}
+
+
 int main() {
     srand((unsigned int)time(NULL));
 
-    int N = 10;
+    int N = 1024;
 
     Matrix A = create_matrix(N, N);
     Matrix B = create_matrix(N, N);
@@ -61,12 +74,23 @@ int main() {
     }
 
     printf("Матрицы успешно созданы и заполнены случайными числами.\n");
-    printf("Матрица A:\n");
-    print_matrix(A);
-    printf("Матрица B:\n");
-    print_matrix(B);
-    printf("Матрица C:\n");
-    print_matrix(C);
+    // printf("Матрица A:\n");
+    // print_matrix(A);
+    // printf("Матрица B:\n");
+    // print_matrix(B);
+    // printf("Матрица C:\n");
+    // print_matrix(C);
+
+    clock_t start = clock();
+    
+    gemm_single_threaded(A, B, C);
+    
+    clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Вычисления завершены.\n");
+    printf("Время выполнения (1 поток): %.4f секунд\n", cpu_time_used);
+
 
     free_matrix(&A);
     free_matrix(&B);
